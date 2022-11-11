4be1e7e282ba4d64e038cb800de8bb98d48eb3d7
programmerQwQ <117915921+programmerQwQ@users.noreply.github.com>
2022-11-11 20:57:18 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,8 +1,14 @@
 function search(){
+  if (document.getElementById("select").value=="百度") {
+    searchOnBaiDu(document.getElementById("text").value);
+  }
   if (document.getElementById("select").value=="必应") {
     searchOnBing(document.getElementById("text").value);
   }
-  console.log(document.getElementById("select").value);
+}
+
+function searchOnBaiDu(value) {
+  window.location.href="https://www.baidu.com/s?wd="+value;
 }
 
 function searchOnBing(value) {
